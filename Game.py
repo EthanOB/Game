a0c0fad4ee25353185da0b36aad01b8c5d2b272f
EthanOB <gameguy0308@gmail.com>
2020-03-21 14:03:49 -0500

    i changed another thing

--- Game.py
+++ Game.py
@@ -1,1 +1,18 @@
-import * from Config
+import Config
+import pygame
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+
+pygame.display.set_caption('Hello World!')
+
+gameDisplay = pygame.display.set_mode((200,200))
+
+pygame.display.set_caption('Tree Game')
+while True: # main game loop
+    gameDisplay.fill((0,0,200))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
